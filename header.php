1b7ac31def2ea3f636d60ba416f4b7cb7114bd21
= <=muzuvajoshua@gmail.com>
2022-01-29 17:17:20 +0300

    Common header page committed by Joshua Muzuva

---
+++ header.php
@@ -0,0 +1,9 @@
+
+ <header>
+        
+    <div class = "header_image">
+        <img width="100px" height="100px" src="n.jpg" alt="ABC"> <figcaption>let us learn together</figcaption>
+    </div >
+    
+    <h1 class="head">KINDER CARE E-LEARNING SYSTEM</h1>
+</header>
