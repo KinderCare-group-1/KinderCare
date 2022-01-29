3b406979fa1186a8654345c3f9870174fd73fc98
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2022-01-29 16:12:03 +0300

    Add files via upload
    Initial index page commit by Loor Jacobson

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
-include"connection.php";
+ include"connection.php";
 
-if(isset($_POST['login'])){
-    $userName= mysqli_real_escape_string($link, $_POST['username']);
-    $password= mysqli_real_escape_string($link, $_POST['password']);
-    $query2="SELECT username, passwords FROM teachers;";
+ if(isset($_POST['login'])){
+   $userName= mysqli_real_escape_string($link, $_POST['username']);
+$password= mysqli_real_escape_string($link, $_POST['password']);
+$query2="SELECT username, passwords FROM teachers;";
 
-    $result=mysqli_query($link, $query2);
-    $resultzo = mysqli_fetch_assoc($result);
-        if($userName==$resultzo['username'] && $password==$resultzo['passwords']){
-        header("Location:register.php");
-        }
+$result=mysqli_query($link, $query2);
+$resultzo = mysqli_fetch_assoc($result);
+if($userName==$resultzo['username'] && $password==$resultzo['passwords']){
+      header("Location:register.php");
+}
     
-
 }
 
-?>
+?> 
 
 
 <!DOCTYPE html>
@@ -25,25 +24,55 @@ if(isset($_POST['login'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <style>
+        /* .container{
+          width: 100%;   
+          margin: 50px auto;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 600px;
+        }
+
+        .container form{
+            width: 600px;
+        }
+
+        .teacher{
+           text-align: center;
+           margin-bottom: 40px;
+         } */
+    </style>
     <title>KCES</title>
 </head>
 
 <body>
 
     <?php require ("header.php"); ?>
-    <h2>Teacher's login page</h2>
-
-    <div>
-<form action="index.php" method ="POST">
-
-<label for="username">USER NAME</label>
-<input type="text" name="username">
-<labe>PASSWORD</label>
-<input type="password" name = "password">
-<button type="submit" name="login">LOG IN </button>
-<p>First time here? <a href="signup.php"> sign up </a> </p> 
-</form>
+   
+    <div  class= "container">
+    
+        <form action="index.php" method ="POST">
+        
+            <div class="login">
+                <h2 class="teacher">Teacher's login page</h2>
+                <label for="username">USER NAME</label>
+                <input type="text" name="username">
+                <label>PASSWORD</label>
+                <input type="password" name = "password">
+                <button type="submit" name="login" >LOG IN </button>
+                <p>First time here? <a href="signup.php"> sign up </a> </p> 
+            </div>
+        </form>
     </div>
+ 
+
+
+
+
 </body>
 
 </html>
