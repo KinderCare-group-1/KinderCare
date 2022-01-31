b342766b644a8cb26e42d93f8920b9950edd8a6e
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2022-01-31 15:05:21 +0300

    Delete header.php

--- header.php
+++
@@ -1,6 +0,0 @@
-    <header class = "header" id = "header">
-    <h1 class = "branding"><span>KINDER CARE<span> E-LEARNING SYSTEM</h1>
-    <div class = "nav">
-        <button class = "btn btn-logout"> Logout </button>
-    </div>
-    </header>
